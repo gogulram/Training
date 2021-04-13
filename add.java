b4f03bbd8e26f1064ce916f101fdc23be39d8cef
gogulram <43104880+gogulram@users.noreply.github.com>
2021-04-13 17:23:22 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,17 @@
+
+package firstpro;
+import java.util.*;
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the numbers:");
+		int num1=sc.nextInt();
+		int num2=sc.nextInt();
+		int res=num1+num2;
+		System.out.println("The result is "+res);
+
+	}
+
+}
